21c6b31639ac8346f56e72ec68b6ed37b5648a1c
Dev Baweja <18ucs081@lnmiit.ac.in>
2021-02-08 09:20:18 +0530

    Negative Count Sort

--- GeeksForGeeks/Sorting/Counting Sort.cpp
+++ GeeksForGeeks/Sorting/Counting Sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int maximum(int A[], int n)
@@ -6,7 +7,14 @@ int maximum(int A[], int n)
     int maxi = INT_MIN;
     for (int i = 0; i < n; i++)
         maxi = max(maxi, A[i]);
-    return maxi + 1;
+    return maxi;
+}
+int minimum(int A[], int n)
+{
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++)
+        mini = min(mini, A[i]);
+    return mini;
 }
 
 void countSort(int A[], int n)
@@ -18,7 +26,7 @@ void countSort(int A[], int n)
         count[i] = 0;
     // Count
     for (int i = 0; i < n; i++)
-        count[A[i]]++;
+        count[A[i] - 1]++;
     // Index
     for (int i = 1; i < maxi; i++)
         count[i] += count[i - 1];
@@ -26,7 +34,7 @@ void countSort(int A[], int n)
     int B[n];
     // Reverse iterate to make sorting stable
     for (int i = n - 1; i >= 0; i--)
-        B[--count[A[i]]] = A[i];
+        B[--count[A[i] - 1]] = A[i];
 
     for (int i = 0; i < n; i++)
         A[i] = B[i];
@@ -34,12 +42,36 @@ void countSort(int A[], int n)
 // Time- O(n+k)
 // k - Range of input
 // Space- O(n+k)
+// Data - [1-k]
+
+void countSortNegative(int A[], int n)
+{
+    int maxi = maximum(A, n);
+    int mini = minimum(A, n);
+    int range = maxi - mini + 1;
+    int count[range];
+    for (int i = 0; i < range; i++)
+        count[i] = 0;
+
+    for (int i = 0; i < n; i++)
+        count[A[i] - mini]++;
+
+    for (int i = 1; i < range; i++)
+        count[i] += count[i - 1];
+
+    int B[n];
+    for (int i = n - 1; i >= 0; i--)
+        B[--count[A[i] - mini]] = A[i];
+
+    for (int i = 0; i < n; i++)
+        A[i] = B[i];
+}
 
 int main()
 {
     int A[] = {1, 4, 1, 2, 7, 5, 2};
     int n = 7;
-    countSort(A, n);
+    countSortNegative(A, n);
     for (int i = 0; i < n; i++)
         cout << A[i] << " ";
     return 0;
